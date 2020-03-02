36cca1a32497cf716c3d7bb121c41829cd75048d
sagitush <44786027+sagitush@users.noreply.github.com>
2020-03-02 21:51:54 +0200

    Add files via upload

---
+++ DesignPatern1.3.20/Decorator/BackgroundFlashing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    class BackgroundFlashing: TypesDecoratorBase
+    {
+        public BackgroundFlashing(IWindow window) : base(window)
+        {
+        }
+
+        public override string GetDetails()
+        {
+            return window.GetDetails() + " Background flashing";
+        }
+    }
+}
---
+++ DesignPatern1.3.20/Decorator/ColorfulFrame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    class ColorfulFrame: TypesDecoratorBase
+    {
+        public ColorfulFrame(IWindow window) : base(window)
+        {
+        }
+
+        public override string GetDetails()
+        {
+            return window.GetDetails() + " colorful frame";
+        }
+    }
+}
